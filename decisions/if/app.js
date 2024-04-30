6f962eee960d10cefe8917af4a498c2f30010210
Ezequiel Madrid <madridezequiel299@gmail.com>
2024-04-30 13:44:03 -0300

    arch gallery + humans decisions

---
+++ decisions/if/app.js
@@ -0,0 +1,27 @@
+/* 
+HUMANS DECISIONS ===> Conditions
+
+(1) Me Despierto ☕
+Si tengo hambre
+    como algo
+Sino tomo café
+
+(2) Miro mi Celu 📱
+Si está por morir
+    lo cargo
+Sino uso Spotify
+
+(3) Salgo al Centro 🚶
+Si está lloviendo
+    llevo un paragüa
+Sino llevo gafas de sol
+
+(4) Estoy en un Bar 🍺
+Si quiero comer
+    pido pizza
+        Sino hay pizza
+            pido hamburguesas
+Sino hay niguno de esos menús
+    pido un trago
+
+*/
---
+++ dom/app/data.js
@@ -0,0 +1,32 @@
+const myData = [
+  {
+    title: "The Eye Base",
+    imageUrl: "assets/img/image1.jpg",
+    content: "New Pyramid",
+  },
+  {
+    title: "Urban Horizon",
+    imageUrl: "assets/img/image2.jpg",
+    content: "New City",
+  },
+  {
+    title: "Metropolis Eclipsed",
+    imageUrl: "assets/img/image3.jpg",
+    content: "Encapsulated City",
+  },
+  {
+    title: "Sustainable Skylines",
+    imageUrl: "assets/img/image4.png",
+    content: "Green Future with Amazing Buildings!",
+  },
+  {
+    title: "The Shelter",
+    imageUrl: "assets/img/image5.jpg",
+    content: "Multiple Hexagonal Arcs",
+  },
+  {
+    title: "Transcendent Cityscapes",
+    imageUrl: "assets/img/image6.jpg",
+    content: "Earth 616",
+  },
+];
