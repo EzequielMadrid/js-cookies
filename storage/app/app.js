bf7bfedd22dc6f4d2906d364fad6f1b5d9ff408b
Ezequiel Madrid <madridezequiel299@gmail.com>
2024-05-15 02:06:20 -0300

    storage updated

--- storage/app/app.js
+++ storage/app/app.js
@@ -1,8 +1,15 @@
 const my_tweet = () => {
   let database = JSON.parse(localStorage.getItem("mensajes")) || [];
-  tweet = prompt("What is happening?");
+  let tweet = prompt("What is happening?");
+
+  while (tweet === "" || tweet === null) {
+    alert("TWEET EMPTY❓");
+    tweet = prompt("What is happening?"); // declaramos tweet de vuelta
+  }
+
   database.push(tweet);
-  localStorage.setItem("mensajes", JSON.stringify(database));
+  localStorage.setItem("posts", JSON.stringify(database));
+
   //console.log(`last tweet: ${tweet}`);
   document.write(`<h1>last tweet: ${tweet}</h1>`);
 };
